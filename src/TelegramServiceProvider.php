3ba260de0710837507caf21f713f1cdece0e5254
Maxim Bondarev <macintosh.way@gmail.com>
2019-04-11 16:17:47 +0500

    add api url

--- src/TelegramServiceProvider.php
+++ src/TelegramServiceProvider.php
@@ -17,7 +17,8 @@ class TelegramServiceProvider extends ServiceProvider
             ->give(function () {
                 return new Telegram(
                     config('services.telegram-bot-api.token'),
-                    new HttpClient()
+                    new HttpClient(),
+                    config('services.telegram-bot-api.url')
                 );
             });
     }
